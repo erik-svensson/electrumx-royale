ff00a9fb6b939aa9d747138eb73c78e2ec4a0985
harzo <harzojunior@gmail.com>
2020-08-18 15:27:05 +0200

    Lower min protocol version to 1.4

--- electrumx/server/session.py
+++ electrumx/server/session.py
@@ -885,7 +885,7 @@ class SessionBase(RPCSession):
 class ElectrumX(SessionBase):
     '''A TCP server that handles incoming Electrum connections.'''
 
-    PROTOCOL_MIN = (2, 0)
+    PROTOCOL_MIN = (1, 4)
     PROTOCOL_MAX = (2, 0)
 
     def __init__(self, *args, **kwargs):
